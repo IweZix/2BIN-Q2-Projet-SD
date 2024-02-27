bcbc2b462a2535ce44839c67ad825850ae618b0c
IweZix <IweZix@users.noreply.github.com>
2024-02-27 16:46:55 +0100

    add: Graph class

---
+++ src/Graph.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.io.File;
+
+public class Graph {
+
+  Graph(File cities, File roads) {
+
+  }
+
+  public void calculerItineraireMinimisantNombreRoutes(String depart, String arrivee) {
+
+  }
+
+  public void calculerItineraireMinimisantKm(String depart, String arrivee) {
+
+  }
+}
